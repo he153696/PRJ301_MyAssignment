23c7583ca8d88484bf50d1ddd638fd857e8ffc73
he153696 <123060084+he153696@users.noreply.github.com>
2023-03-20 12:41:09 +0700

    create login page and Servlet for login and logout

---
+++ Assignment/src/java/controller/instructor/auth/LoginController.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.instructor.auth;
+
+import dal.AccountDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import model.Account;
+
+/**
+ *
+ * @author datng
+ */
+public class LoginController extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("view/íntructor/auth/login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        AccountDBContext db = new AccountDBContext();
+        Account acc = db.getAccountByEmailPassword(email, password);
+        if (acc != null) {
+            request.getSession().setAttribute("account", acc);
+            request.getSession().setAttribute("isInstructor", db.isInstructor(acc.getId()));
+            response.sendRedirect(request.getContextPath()+"/home");
+        } else {
+            request.setAttribute("message", "Email or password wrong, try again");
+            request.setAttribute("email", email);
+            request.setAttribute("password", password);
+            request.getRequestDispatcher("views/instructor/auth/login.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
